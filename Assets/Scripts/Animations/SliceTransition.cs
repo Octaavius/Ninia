bc9e3b21146dfea58a6f96e743ed02ffb764802b
AndriiPokora <andpokora@gmail.com>
2024-07-16 12:43:59 +0200

    Update SliceTransition.cs

--- Assets/Scripts/Animations/SliceTransition.cs
+++ Assets/Scripts/Animations/SliceTransition.cs
@@ -1,16 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 public class SliceTransition : MonoBehaviour
 {
     public RectTransform uiElementToBottom;
     public RectTransform uiElementToUp;
+    public AnimationCurve customCurve;
     public GameObject slice;
     public Vector3 startPosition;
     public Vector3 endPosition;
-    private readonly float screenHeight = Screen.height;
-    private readonly float screenWidth = Screen.width;
+    private float screenHeight;
+    private float screenWidth;
     [SerializeField]private float movingTime = 1f;
     [SerializeField]private float slicingTime = 0.5f;
     
+    void Start() {
+        screenHeight = Display.main.systemHeight;
+        screenWidth = Display.main.systemWidth;
+    }
     public void PlayAnimation() {
         // Set the object's position to the start position
         slice.transform.position = startPosition;
@@ -31,7 +37,7 @@ public class SliceTransition : MonoBehaviour
         // Move the UI element down by half the screen height and to the left by the screen width using DoTween
         Debug.Log(-screenWidth);
         LeanTween.move(uiElementToBottom, new Vector2(-screenWidth, -screenHeight * 0.327f), 1f)
-            //.SetEase(Ease.Linear) // Optional: set the ease type
+            .setEase(customCurve) // Optional: set the ease type
             .setOnComplete(() => Debug.Log("UI moved down.")); // Optional: callback when animation completes
     }
     void UIMoveToUp()
@@ -39,7 +45,7 @@ public class SliceTransition : MonoBehaviour
         // Move the UI element down by half the screen height and to the left by the screen width using DoTween
         Debug.Log(screenWidth);
         LeanTween.move(uiElementToUp, new Vector2(screenWidth, screenHeight * 0.327f), 1f)
-            //.SetEase(Ease.Linear) // Optional: set the ease type
+            .setEase(customCurve) // Optional: set the ease type
             .setOnComplete(() => Debug.Log("UI moved up.")); // Optional: callback when animation completes
     }
 
